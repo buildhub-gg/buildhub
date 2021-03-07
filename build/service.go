6b61c33d387c3c934c251ae08563256262d04bc2
TheGrizzlyDev <tonio9681@gmail.com>
2021-03-07 15:36:29 +0000

    cleaned up a few things

--- build/service.go
+++ build/service.go
@@ -7,10 +7,18 @@ type Service struct {
 }
 
 func (s *Service) Create(build *Build) (string, error) {
-	if build.Name == "" {
-		return "", errors.New("build name cannot be null")
+	err := s.validate(build)
+	if err != nil {
+		return "", err
 	}
 	return s.repo.Create(build)
+}
+
+func (s *Service) validate(build *Build) error {
+	if build.Name == "" {
+		return errors.New("build name cannot be null")
+	}
+	return nil
 }
 
 func NewService(repo BuildRepository) *Service {
--- item/repository.go
+++ item/repository.go
@@ -8,26 +8,6 @@ import (
 	_ "embed" // Allows go 1.16 embed to work
 )
 
-type AttributeType string
-
-const (
-	STRING AttributeType = "STRING"
-	INT AttributeType = "INT"
-	FLOAT AttributeType = "FLOAT"
-	BOOL AttributeType = "BOOL"
-)
-
-type AttributeSpec struct {
-	ID string `yaml:"id"`
-	Type AttributeType `yaml:"type"`
-	Definitions interface{} `yaml:"definitions,omitempty"` //They currently exist but are unused
-}
-
-type ItemSpec struct {
-	ID string `yaml:"id"`
-	Attributes []AttributeSpec `yarm:"attributes,omitempty"`
-}
-
 var (
 	//go:embed data/super-mario.yml
 	supermario []byte
