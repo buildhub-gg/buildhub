f2d09f5c6a826341a5d41e9fa7fc63ee27dc805d
TheGrizzlyDev <tonio9681@gmail.com>
2021-03-09 22:37:30 +0000

    Added attributes to specs (#4)

--- graph/converter.go
+++ graph/converter.go
@@ -1,8 +1,11 @@
 package graph
 
 import (
+	"fmt"
+
 	buildPackage "github.com/TheGrizzlyDev/buildhub/build"
 	"github.com/TheGrizzlyDev/buildhub/graph/model"
+	"github.com/TheGrizzlyDev/buildhub/item"
 )
 
 func convertInputBuildToOutputBuild(ID string, in *model.InputBuild) *model.Build  {
@@ -51,3 +54,107 @@ func convertInputBuildToEntityBuild(in *model.InputBuild) *buildPackage.Build {
 		Items: buildItems,
 	}
 }
+
+func convertItemSpecToGraphql(in *item.ItemSpec) (*model.ItemSpec, error) {
+	attributes := []model.AttributeSpec{}
+	for _, attribute := range in.Attributes {
+		convertedAttribute, err := convertAttributeSpecToGraphql(&attribute)
+		if err != nil {
+			return nil, err
+		}
+		attributes = append(attributes, convertedAttribute)
+	}
+	
+	return &model.ItemSpec{
+		ID: in.ID,
+		Attributes: attributes,
+	}, nil
+}
+
+func convertAttributeSpecToGraphql(in *item.AttributeSpec) (model.AttributeSpec, error) {
+	switch itemType := in.Type; itemType {
+	case item.BOOL:
+		return convertBooleanAttributeSpecToGraphql(in), nil
+	case item.STRING:
+		return convertStringAttributeSpecToGraphql(in), nil
+	case item.FLOAT:
+		return convertFloatAttributeSpecToGraphql(in), nil
+	case item.INT:
+		return convertIntAttributeSpecToGraphql(in), nil
+	default:
+		return nil, fmt.Errorf("Cannot find spec of type %s", itemType)
+	}
+}
+
+func convertBooleanAttributeSpecToGraphql(in *item.AttributeSpec) *model.BooleanAttributeSpec {
+	out := &model.BooleanAttributeSpec {
+		ID: in.ID,
+	}
+	
+	if defaultValue, ok := in.Definitions["default"]; ok {
+		defaultValueBool := defaultValue.(bool)
+		out.Default = &defaultValueBool
+	}
+
+	return out
+}
+
+func convertStringAttributeSpecToGraphql(in *item.AttributeSpec) *model.StringAttributeSpec {
+	out := &model.StringAttributeSpec {
+		ID: in.ID,
+	}
+	
+	if defaultValue, ok := in.Definitions["default"]; ok {
+		defaultValueString := defaultValue.(string)
+		out.Default = &defaultValueString
+	}
+
+	return out
+}
+
+func convertIntAttributeSpecToGraphql(in *item.AttributeSpec) *model.IntAttributeSpec {
+	out := &model.IntAttributeSpec {
+		ID: in.ID,
+	}
+	
+	if defaultValue, ok := in.Definitions["default"]; ok {
+		defaultValueInt := defaultValue.(int)
+		out.Default = &defaultValueInt
+	}
+	
+	if min, ok := in.Definitions["min"]; ok {
+		minValue := min.(int)
+		out.Min = &minValue
+	}
+	
+	if max, ok := in.Definitions["max"]; ok {
+		maxValue := max.(int)
+		out.Max = &maxValue
+	}
+
+	return out
+}
+
+func convertFloatAttributeSpecToGraphql(in *item.AttributeSpec) *model.FloatAttributeSpec {
+	out := &model.FloatAttributeSpec {
+		ID: in.ID,
+	}
+	
+	if defaultValue, ok := in.Definitions["default"]; ok {
+		defaultValueFloat := defaultValue.(float64)
+		out.Default = &defaultValueFloat
+	}
+	
+	if min, ok := in.Definitions["min"]; ok {
+		minValue := min.(float64)
+		out.Min = &minValue
+	}
+	
+	if max, ok := in.Definitions["max"]; ok {
+		maxValue := max.(float64)
+		out.Max = &maxValue
+	}
+
+	return out
+}
+
--- item/model.go
+++ item/model.go
@@ -12,7 +12,7 @@ const (
 type AttributeSpec struct {
 	ID string `yaml:"id"`
 	Type AttributeType `yaml:"type"`
-	Definitions interface{} `yaml:"definitions,omitempty"` //They currently exist but are unused
+	Definitions map[string]interface{} `yaml:"definitions,omitempty"` //They currently exist but are unused
 }
 
 type ItemSpec struct {
